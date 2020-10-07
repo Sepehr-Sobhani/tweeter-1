337c1b6d2bb7d9d99049e98a5c7006b79a11d079
ebussiere <eddy@embtech.ca>
2020-10-07 16:48:47 +0000

    initial

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,107 @@
+const maxTweetLength = 140;
+const count = document.querySelector("#count");
+const $tweetContainer = document.querySelector(".tweet-container");
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  count.value = maxTweetLength;
+  $("#tweet-text").on('input', function() {
+    let charCount = this.value.length;
+    count.value = maxTweetLength - charCount;
+    if (charCount > maxTweetLength) {
+      $(count).css("color", "red");
+    } else {
+      $(count).css("color", "black");
+    }
+  });
+
+  $("#tweet-btn").on('click', function(event) {
+    event.preventDefault();
+
+  });
+
+
+
+
+
+
+
+});
+
+const generateTweets = function() {
+
+};
+
+
+const renderTweets = function(tweets) {
+  for (const key in tweets) {
+    const tweet = tweets[key];
+    const tweetToRender = createTweetElement(tweet);
+    $(".tweet-container").append(tweetToRender);
+  }
+};
+
+const createTweetElement = function(tweet) {
+  let $tweet = ` <article class="tweet">
+        <div class="tweet-header">
+          <div class="author">
+            <div class="author-avatar">
+            <img src="${tweet.user.avatars}" alt="Girl in a jacket" width="50" height="50">
+              
+            </div>
+            <div class="author-name">
+              ${tweet.user.name}
+            </div>
+          </div>
+          <div class="author-handle">
+            ${tweet.user.handle}
+          </div>
+        </div>
+        <div class="tweet-body">
+          <p>${tweet.content.text}</p>
+        </div>
+        <div class="tweet-footer">
+          <div class="tweet-age">
+            <p>${tweet.created_at}</p>
+          </div>
+          <div class="tweet-actions">
+            <i class="fas fa-flag fa-xs"></i>
+            <i class="fas fa-retweet fa-xs"></i>
+            <i class="fas fa-heart fa-xs"></i>
+          </div>
+        </div>
+      </article>`;
+  return $tweet;
+};
+
+renderTweets(data);
